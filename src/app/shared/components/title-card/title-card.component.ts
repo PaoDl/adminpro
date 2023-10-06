1f98a37d0ec1bd4fdb9dacaa4989ecca5e696109
Paola Dl <kath.pao.acgmail.com>
2023-10-05 19:07:07 -0600

    feat: add title card and profile

--- src/app/shared/components/title-card/title-card.component.ts
+++ src/app/shared/components/title-card/title-card.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'title-card',
+  selector: 'shared-title-card',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './title-card.component.html',
-  styleUrls: ['./title-card.component.css']
+
 })
 export class TitleCardComponent {
+  @Input()
+  title!: string;
+
+  @Input()
+  topMargin: string = 'mt-6';
+
+  get margin() {
+    return {
+      topMargin: true
+    }
+  }
 
 }
