9148f60a2dde6469d29e32ac00341cce08a4f703
Paola Dl <kath.pao.acgmail.com>
2023-10-19 11:39:07 -0600

    gets biome diet

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { BiomeComponent } from './pages/biome/biome.component';
 
 import { SpeciesComponent } from './pages/species/species.component';
+import { DietComponent } from './pages/diet/diet.component';
 
 @NgModule({
   declarations: [],
